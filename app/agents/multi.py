d66d3e8a65d5b685a11e1589f4fce2060469f157
Trey Saddler <treyosaddler@gmail.com>
2024-07-25 15:16:45 -0400

    Refactor app and rag-test to ToxPipe repo

--- app/agents/multi.py
+++
@@ -1,3 +0,0 @@
-def run_parallel(self=None, prompt="", user_id=""):
-    outputs = self.agent_with_chat_history.invoke({"input": prompt},  config={"configurable": {"conversation_id": f"{user_id}", "user_id": f"{user_id}"}},)
-    return([outputs["output"]])
