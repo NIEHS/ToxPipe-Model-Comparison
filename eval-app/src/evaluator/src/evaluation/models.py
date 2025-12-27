a94bac7860dba1108d7d66691cf682c455c99060
Amlan Talukder <amlanaccount@gmail.com>
2025-12-27 09:18:27 -0500

    Updated langchain to facilitate reasoning_effort parameter for gpt-5

--- eval-app/src/evaluator/src/evaluation/models.py
+++ eval-app/src/evaluator/src/evaluation/models.py
@@ -8,7 +8,7 @@ truststore.inject_into_ssl()
 cert_path = str(Path(Config.DIR_HOME / 'certs/NIH-FULL.pem'))
 http_client = httpx.Client(verify=cert_path)
 
-def createOpenAIModel(model_name, temperature):
+def createOpenAIModel(model_name, temperature, **kwargs):
     
     return ChatOpenAI(
         model=model_name,
@@ -19,5 +19,6 @@ def createOpenAIModel(model_name, temperature):
         timeout=None,
         max_retries=10,
         seed=1000,
-        http_client=http_client
+        http_client=http_client,
+        **kwargs
         )
